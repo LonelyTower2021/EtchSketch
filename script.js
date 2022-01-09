1be3fea6f46fd4b35ea262cc6b902839b69186f5
Alonelytower <lonelytower2021@gmail.com>
2022-01-09 13:16:08 -0800

    Add hover effect to grid.

--- script.js
+++ script.js
@@ -4,9 +4,14 @@ function setUpGrid(e) {
         for (let i = 0; i < 16 ; i++) {
             gridCell = document.createElement('div')
             gridCell.classList.add("grid_cell")
+            gridCell.addEventListener("mouseover", changeGridCellColor)
             grid.appendChild(gridCell)
         }
     }
+}
+
+function changeGridCellColor(e) {
+    this.setAttribute('style', 'background-color: black');
 }
 
 window.addEventListener('load', setUpGrid);
