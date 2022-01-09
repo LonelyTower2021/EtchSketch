d42f4f873f982e1a70f0708bea02ee17c5163f67
Alonelytower <lonelytower2021@gmail.com>
2022-01-09 13:40:50 -0800

    Add button to reset grid.

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function setUpGrid(e) {
     grid = document.querySelector('.grid_container')
+    populateGrid(grid);
+}
+
+function populateGrid(grid) {
     for (let i = 0; i < 16; i++) {
         for (let i = 0; i < 16 ; i++) {
             gridCell = document.createElement('div')
@@ -14,4 +18,15 @@ function changeGridCellColor(e) {
     this.setAttribute('style', 'background-color: black');
 }
 
+function clearGrid(e) {
+    grid = document.querySelector('.grid_container')
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    }
+    populateGrid(grid);
+}
+
 window.addEventListener('load', setUpGrid);
+
+const btn = document.querySelector('button')
+btn.addEventListener('click', clearGrid);
