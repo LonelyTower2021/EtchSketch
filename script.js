f5ca39d5a9491da5f1b2de5aa63dd95366c5881f
Alonelytower <lonelytower2021@gmail.com>
2022-01-09 21:53:45 -0800

    Add button to both clear grid, and allow user to enter a custom grid size.

--- script.js
+++ script.js
@@ -3,11 +3,15 @@ function setUpGrid(e) {
     populateGrid(grid);
 }
 
-function populateGrid(grid) {
-    for (let i = 0; i < 16; i++) {
-        for (let i = 0; i < 16 ; i++) {
+function populateGrid(grid, gridRowCount=4) {
+    let cellDimension = (1 / gridRowCount) * 100;
+    console.log(cellDimension);
+    for (let i = 0; i < gridRowCount; i++) {
+        for (let j = 0; j < gridRowCount ; j++) {
             gridCell = document.createElement('div')
             gridCell.classList.add("grid_cell")
+            gridCell.style.width = `${cellDimension}%`
+            gridCell.style.height = `${cellDimension}%`
             gridCell.addEventListener("mouseover", changeGridCellColor)
             grid.appendChild(gridCell)
         }
@@ -15,18 +19,24 @@ function populateGrid(grid) {
 }
 
 function changeGridCellColor(e) {
-    this.setAttribute('style', 'background-color: black');
+    this.style.backgroundColor = "black";
 }
 
-function clearGrid(e) {
+function updateGrid(e) {
     grid = document.querySelector('.grid_container')
     while (grid.firstChild) {
         grid.removeChild(grid.firstChild);
     }
-    populateGrid(grid);
+
+    let newGridSize;
+    do {
+        newGridSize = parseInt(prompt("Please enter a custom grid size (Between 1 and 100):"));
+    } while (newGridSize < 1 || newGridSize > 100);
+
+    populateGrid(grid, parseInt(newGridSize));
 }
 
 window.addEventListener('load', setUpGrid);
 
 const btn = document.querySelector('button')
-btn.addEventListener('click', clearGrid);
+btn.addEventListener('click', updateGrid);
